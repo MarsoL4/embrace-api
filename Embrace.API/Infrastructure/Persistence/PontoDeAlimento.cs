a74299c429cd6c28df7bd1f521f4387cf62ad2ac
Enzo Marsola <enzogmarsola@gmail.com>
2025-05-29 20:09:57 -0300

    Remove os atributos de Latitude e Longitude dos Pontos de Alimento

--- Embrace.API/Infrastructure/Persistence/PontoDeAlimento.cs
+++ Embrace.API/Infrastructure/Persistence/PontoDeAlimento.cs
@@ -5,8 +5,6 @@
         public long Id { get; set; }
         public string NomeLocal { get; set; }
         public string Endereco { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
         public int Capacidade { get; set; } // quantos kits por exemplo
     }
 }
